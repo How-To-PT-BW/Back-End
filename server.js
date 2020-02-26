1ce56b9a3c638d5a21d8494bc4aecf484ec0f949
cladams0203 <cladams0203@gmail.com>
2020-02-26 14:10:13 -0500

    created modal and routes for user register and login

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 
+const userRouter = require('./users/usersRouter')
+
 
 const server = express()
 
 server.use(helmet())
 server.use(express.json())
 server.use(cors())
+
+server.use('/users', userRouter)
 
 server.get('/', (req,res) => {
     res.status(200).json({message: 'Welcome to the How-To Api'})
